a12c9d86a86118270b38e3c92663018b66a7f7fd
Willian santos <wsantosdelima44@gmail.com>
2025-01-25 13:23:12 -0300

    Último commit

--- main.py
+++ main.py
@@ -1,1 +1,42 @@
-print('Jogo da forca')
+import random 
+
+palavras = ['python', 'programacao', 'computador', 'aula', 'variavel']
+
+palavra_sorteada = random.choice(palavras)
+
+palavra_escondida = '-' * len(palavra_sorteada)
+
+letras_adivinhadas = []
+
+max_tentativas = 6
+
+while True:
+    print(palavra_escondida)
+
+    letra = input('Digite uma letra: ')
+
+    if letra in letras_adivinhadas:
+        print('você já digitou essa letra')
+        continue
+
+    letras_adivinhadas.append(letra)
+
+    if letra in palavra_sorteada:
+        lista = []
+        for indice in range(len(palavra_sorteada)):
+            if letra == palavra_sorteada[indice]:
+                lista.append(letra)
+            else:
+                lista.append(palavra_escondida[indice])
+        palavra_escondida = ''.join(lista)
+
+    else:
+        max_tentativas -= 1
+        print(f'letra n encontrada. Vc tem mais {max_tentativas} tentativas')
+
+    if palavra_escondida == palavra_sorteada:
+        print('Parabénssssss, você ganhou!!!')
+        break
+    elif max_tentativas == 0:
+        print(f'Você perdeu. A palavra era {palavra_sorteada}.')
+        break
